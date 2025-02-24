55ff711c9e73377a51feac2fdaf74d971a0f367f
VytautasOz <patrikasv@gmail.com>
2025-02-24 21:36:06 +0200

    Hibnernate_Uzduotis1

--- src/main/java/org/example/entity/Darbuotojas.java
+++ src/main/java/org/example/entity/Darbuotojas.java
@@ -1,35 +1,35 @@
-//package org.example.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//import lombok.ToString;
-//
-//@Getter
-//@Setter
-//@ToString
-//@Entity
-//@Table(name = "darbuotojas")
-//
-//public class Darbuotojas {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//    private String vardas;
-//    private String pavarde;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "skyrius_id")
-//    private Skyrius skyrius;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "projektas_id")  // Foreign key column
-//    private Projektas projektas;
-//
-//    public Darbuotojas(String vardas, String pavarde, Skyrius skyrius, Projektas projektas) {
-//        this.vardas = vardas;
-//        this.pavarde = pavarde;
-//        this.skyrius = skyrius;
-//        this.projektas = projektas;
-//    }
-//}
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "darbuotojas")
+
+public class Darbuotojas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String vardas;
+    private String pavarde;
+
+    @ManyToOne
+    @JoinColumn(name = "skyrius_id")
+    private Skyrius skyrius;
+
+    @ManyToOne
+    @JoinColumn(name = "projektas_id")  // Foreign key column
+    private Projektas projektas;
+
+    public Darbuotojas(String vardas, String pavarde, Skyrius skyrius, Projektas projektas) {
+        this.vardas = vardas;
+        this.pavarde = pavarde;
+        this.skyrius = skyrius;
+        this.projektas = projektas;
+    }
+}
--- src/main/java/org/example/entity/Projektas.java
+++ src/main/java/org/example/entity/Projektas.java
@@ -1,29 +1,29 @@
-//package org.example.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//import lombok.ToString;
-//
-//import java.util.List;
-//
-//
-//@Getter
-//@Setter
-//@ToString
-//@Entity
-//@Table(name = "projektas")
-//public class Projektas {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//
-//    private String pavadinimas;
-//
-//    @OneToMany(mappedBy = "projektas", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-//    private List<Darbuotojas> darbuotojai;
-//
-//    public Projektas(String pavadinimas) {
-//        this.pavadinimas = pavadinimas;
-//    }
-//}
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "projektas")
+public class Projektas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String pavadinimas;
+
+    @OneToMany(mappedBy = "projektas", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Darbuotojas> darbuotojai;
+
+    public Projektas(String pavadinimas) {
+        this.pavadinimas = pavadinimas;
+    }
+}
